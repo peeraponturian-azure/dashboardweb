eb1f06702b5eb3203ec53c5e91ad028c1f470849
miccellsupport <103086983+miccellsupport@users.noreply.github.com>
2023-05-06 14:16:52 +0700

    Mile : Update First dev

---
+++ Pages/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dashboard.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
